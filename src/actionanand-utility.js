2af8200bda06fdb9569308ce2e647773668e43b1
Anand raja <meetanandns@gmail.com>
2022-08-10 15:58:29 +0000

    refactoring

--- src/actionanand-utility.js
+++ src/actionanand-utility.js
@@ -1,14 +1,14 @@
 // Anything exported from this file is importable by other in-browser modules.
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject } from 'rxjs';
 
-let qwe = "";
+let rxjxData = '';
 
 const apiCache = new Map();
 // Anything exported from this file is importable by other in-browser modules.
 export function getData(url) {
   const data = apiCache.get(url);
   if (data) {
-    console.log("cached api");
+    console.log('cached api');
     return Promise.resolve(data);
   }
   return new Promise((resolve) => {
@@ -18,16 +18,18 @@ export function getData(url) {
         result: 10,
       };
       apiCache.set(url, apiResponse);
-      console.log("actual api");
+      console.log('actual api');
       resolve(apiResponse);
     }, 2000);
   });
 }
 
-export const state$ = new BehaviorSubject({});
+export const state$ = new BehaviorSubject({data: rxjxData});
 
-window.addEventListener("vanilla", (evnt) => {
-  const fields = ["detail"];
+window.addEventListener('vanilla', (evnt) => {
+  const fields = ['detail'];
   console.log(evnt);
   console.log(evnt[fields[0]]);
+  // rxjxData = evnt[fields[0]];
+  // state$.next({data: rxjxData});
 });
